c124a1ee1b100213182f11ba9b2016a5430d5f3c
Naser Fernandez <naser_015@hotmail.com>
2022-12-22 15:21:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const displayValorActual = document.getElementById('valor-actual');
+const displayValorAnterior = document.getElementById('valor-anterior');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperador = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior,displayValorActual);
+
+botonesNumeros.forEach(boton =>{
+    boton.addEventListener('click', () =>display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperador.forEach(boton =>{
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
